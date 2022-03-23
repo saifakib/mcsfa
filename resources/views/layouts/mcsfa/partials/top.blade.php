6500787de1175c5a2b0aeda6e2d41dd2a6e48f09
saifakib <mdsaifakib@gmail.com>
2022-03-23 12:11:00 +0600

    feat salarybill ui

--- resources/views/layouts/mcsfa/partials/top.blade.php
+++ resources/views/layouts/mcsfa/partials/top.blade.php
@@ -18,6 +18,7 @@
     <link rel="stylesheet" href="{{asset('/')}}css/scrolltop.css">
     <link rel="stylesheet" href="{{asset('/')}}css/responsivestyle.css">
     <link rel="stylesheet" href="{{asset('/')}}css/toastr.css">
+    <link rel="stylesheet" href="{{asset('/')}}css/custom.css">
     {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> --}}
     <style>
         /* Montserrat */
--- resources/views/layouts/mcsfa/salaryBill.blade.php
+++ resources/views/layouts/mcsfa/salaryBill.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.mcsfa.app')
 @section('content')
 
+
 <div id="contentWrapper" style="font-family: Montserrat">
     <!-- Content Header (Page header) -->
     <div class="content-wrapper">
@@ -12,13 +13,13 @@
                 <div class="panel" style="margin-top:-20px;">
                     <div class="panel-content" style="font-family: Montserrat" id="printarea">
                         <center>
-                            <h3 style="font-family: Montserrat;font-weight: bold">Bangladesh Bridge Authority</h3>
+                            <h3 class="cls" style="font-family: Montserrat;font-weight: bold">Bangladesh Bridge Authority</h3>
                             <h5 style="font-family: Montserrat;font-weight: bold">Setu Bhaban, Banani, Dhaka</h5><br>
                             <h4 style="font-family: Montserrat;font-weight: bold">Individual Salary Bill for the month of January-2022</h4>
                         </center>
                         <br>
                         <div class="row" style="padding: 30px">
-                            <div class="col-lg-6 col-md-6 col-12">
+                            <div class="col-lg-6 col-md-6 col-sm-6 col-6">
                                 <div class="row">
                                     <div class="col-md-4 col-sm-4">
                                         <label>Employee No :</label>
@@ -44,7 +45,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-lg-6 col-md-6 col-12">
+                            <div class="col-lg-6 col-md-6 col-6">
                                 <div class="row">
                                     <div class="col-md-4 col-sm-4">
                                         <label>Designation :</label>
@@ -73,8 +74,8 @@
                         </div>
                         <div style="border-color: black;border-bottom-style: solid; border-bottom-width: thin;"></div>
                         <br>
-                        <div class="row row-cols-2 row-cols-lg-2" style="padding: 30px">
-                            <div class="col-md-5" style="border-right: 1px solid black">
+                        <div class="row" style="padding: 30px">
+                            <div class="col-md-5">
                                 <label><h4>PAY</h4></label>
                                 <div class="row">
                                     <div class="col-md-8">
@@ -112,13 +113,32 @@
                                     <div class="col-md-3">
                                         <span style="margin-left: 50px;">19150.00</span>
                                     </div>
-                                    
                                 </div>
                             </div> 
-                            
-                            <div class="col-md-7">
+                            <div class="col-md-7" style="padding-left: 30px; border-left: 1px solid black">
                                 <label><h4>DEDUCTION</h4></label>
                                 <div class="row">
+                                    <div class="col-md-8">
+                                        <label>Basic </label>
+                                    </div>
+                                    <div class="col-md-1">:</div>
+                                    <div class="col-md-3">
+                                        <span style="margin-left: 50px;">39150.00</span>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <label>Basic </label>
+                                    </div>
+                                    <div class="col-md-1">:</div>
+                                    <div class="col-md-3">
+                                        <span style="margin-left: 50px;">39150.00</span>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <label>Basic </label>
+                                    </div>
+                                    <div class="col-md-1">:</div>
+                                    <div class="col-md-3">
+                                        <span style="margin-left: 50px;">39150.00</span>
+                                    </div>
                                     <div class="col-md-8">
                                         <label>Basic </label>
                                     </div>
@@ -154,10 +174,82 @@
                                     <div class="col-md-3">
                                         <span style="margin-left: 50px;">19150.00</span>
                                     </div>
-                                    
                                 </div>
                             </div> 
                         </div>
+                        <div class="row">
+                            <div class="col-md-5">
+                                <div style="border-color: black;border-bottom-style: dotted; border-bottom-width: thin;"></div>
+                                <div class="row">
+                                    <div class="col-md-8">
+                                        <label style="margin-left: 30px">Total Payable </label>
+                                    </div>
+                                    <div class="col-md-1"></div>
+                                    <div class="col-md-3">
+                                        <span style="margin-left: 50px;">309150.00</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-7">
+                                <div style="border-color: black;border-bottom-style: dotted; border-bottom-width: thin;"></div>
+                                <div class="row">
+                                    <div class="col-md-8">
+                                        <label style="margin-left: 30px">Total Deduction </label>
+                                    </div>
+                                    <div class="col-md-1"></div>
+                                    <div class="col-md-3">
+                                        <span style="margin-left: 50px;">309150.00</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <br>
+                        <div class="row">
+                            <div class="col-md-4"></div>
+                            <div class="col-md-8">
+                                <label style="margin-right: 25px">Net Payable:</label>
+                                <span><label>5674567.00</label></span>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-4"></div>
+                            <div class="col-md-8">
+                                <label style="margin-right: 25px">In Word:</label>
+                                <span><label>Fifty Five Thousand Eight Hundred Seventy Seven</label></span>
+                            </div>
+                        </div>
+                        <br><br>
+                        <div class="row">
+                            <div class="col-md-4"></div>
+                            <div class="col-md-5">
+                                <table class="table transparentbg">
+                                    <thead>
+                                        <tr style="font-family:Montserrat">
+                                            <th style="text-align: center;border: none"><span style="color: #333">Signature</span><span>----------------------------------------------</span><br><th>
+                                         </tr>
+                                        <tr style="font-family:Montserrat">
+                                            <th style="text-align: center;border: none"><span style="color: #333">Date</span><span>------------------------------------------------------</span><br><th>
+                                        </tr>
+                                    </thead>
+                                </table>
+                            </div>
+                            <div class="col-md-3" style="border: 1px solid black; height: 100px; width: 100px; margin-bottom: 15px;">
+                                <h6></h6>
+                            </div>
+                        </div>
+                        <br><br>
+                        <table class="table transparentbg">
+                            <thead>
+                                <tr style="font-family:Montserrat">
+                                    <th style="text-align: center;border: none"><span>-----------------</span><br><span style="color: #333">Prepared By</span></th>
+                                    <th style="text-align: center;border: none"></th>
+                                    <th style="text-align: center;border: none"><span>-----------------</span><br><span style="color: #333">Checked By</span></th>
+                                    <th style="text-align: center;border: none"><span>-----------------</span><br><span style="color: #333">Approved By</span></th>
+                                    <th style="text-align: center;border: none"></th>
+                                    <th style="text-align: center;border: none"></th>
+                                </tr>
+                            </thead>
+                        </table>
                     </div>
                 </div>
             </div>
